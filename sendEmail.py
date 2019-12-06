86a9873ecc2159914b8e68b45f056652929a72de
ndfinelli <nfinelli@umich.edu>
2019-12-06 14:38:48 -0500

    Update sendEmail.py

--- sendEmail.py
+++ sendEmail.py
@@ -1,25 +1,24 @@
 import smtplib
 
-smtpUser = "brownbanana1234@gmail.com"
-smtpPass = "oveawleewhsvgzsg"
+def sendEmail(address2send2, subject= "Are my bananas brown yet?", body):
+  smtpUser = "brownbanana1234@gmail.com"
+  smtpPass = "oveawleewhsvgzsg"
 
-# Address we want to send the email to
-toAdd = "ndfinelli@gmail.com" 
-fromAdd = smtpUser
+  # Address we want to send the email to
+  toAdd = address2send2 
+  fromAdd = smtpUser
 
-subject = "Are my bananas brown yet?"
-header = "To: " + toAdd + "\n" + "From: " + fromAdd + "\n" + "Subject: " + subject
-body = "your bananas are good to go dude!"
+  header = "To: " + toAdd + "\n" + "From: " + fromAdd + "\n" + "Subject: " + subject
 
-print (header + "\n" + body)
+  print (header + "\n" + body)
 
-s = smtplib.SMTP("smtp.gmail.com", 587)
+  s = smtplib.SMTP("smtp.gmail.com", 587)
 
-s.ehlo()
-s.starttls()
-s.ehlo()
+  s.ehlo()
+  s.starttls()
+  s.ehlo()
 
-s.login(smtpUser, smtpPass)
-s.sendmail(fromAdd, toAdd, header + "\n\n" + body)
+  s.login(smtpUser, smtpPass)
+  s.sendmail(fromAdd, toAdd, header + "\n\n" + body)
 
-s.quit()
+  s.quit()
