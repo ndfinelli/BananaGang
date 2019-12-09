5f8f25d96634b13601c8d13f95ab4afed0d5d8b4
Chelsie Eiden <ceiden@umich.edu>
2019-12-08 22:04:56 -0500

    change color

--- cropDataset.py
+++ cropDataset.py
@@ -6,4 +6,5 @@ for root, dirs, files in os.walk('new_bananas/'):
 	for directory in dirs:
 		for file in os.listdir('new_bananas/' + directory):
 			img = cv2.imread('new_bananas/' + directory + '/' + file)
-			crop_image(img, 'cropped_bananas/' + directory + '/' + file)
+			img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+			crop_image(img_rgb, 'cropped_bananas/' + directory + '/' + file)
