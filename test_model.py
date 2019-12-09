46f5db12b49dea3ce1386d7d8cf41e41b23ee3d3
ndfinelli <nfinelli@umich.edu>
2019-12-09 16:14:41 -0500

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -10,17 +10,12 @@ def set_parameter_requires_grad(model):
     for param in model.parameters():
         param.requires_grad = False
 
-img = cv2.imread('cropped_bananas/banana_semi_green/right11.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+img = cv2.imread('cropped.png')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 img = (img - np.mean(img)) / np.std(img)
 img = cv2.resize(img, (224,224))
 
-model = models.squeezenet1_0(pretrained=True)
-set_parameter_requires_grad(model)
-model.classifier[1] = nn.Conv2d(512, 4, kernel_size=(1,1), stride=(1,1))
-model.num_classes = 4
-
-model.load_state_dict(torch.load("saved_models/new_bananas.pickle"))
+model = torch.load("saved_models/new_bananas.pickle")
 
 with torch.no_grad():
 	right_img = np.array(img)
